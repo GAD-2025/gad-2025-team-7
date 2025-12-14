34ec1bc7d8cf2d811c4d1cb98c6bb89feeb73879
Kim Kyeongphil <kfeeel20@naver.com>
2025-12-14 23:31:32 +0900

    feat: Add MainBackground component based on Figma design

--- one_day_fe_react/src/MainLayout.js
+++ one_day_fe_react/src/MainLayout.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import ProfileHeader from './ProfileHeader';
+import MainBackground from './components/MainBackground';
 import './MainLayout.css';
 
 const MainLayout = () => {
     return (
-        <div className="main-layout">
-            <header className="main-header">
-                <ProfileHeader />
-            </header>
-            <main className="main-content">
-                <Outlet />
-            </main>
-        </div>
+        <MainBackground>
+            <div className="main-layout">
+                <header className="main-header">
+                    <ProfileHeader />
+                </header>
+                <main className="main-content">
+                    <Outlet />
+                </main>
+            </div>
+        </MainBackground>
     );
 };
 
