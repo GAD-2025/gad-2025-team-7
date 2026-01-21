32572d32546a67c36584e98b10930a3befd6c7f2
Kim Kyeongphil <kfeeel20@naver.com>
2026-01-21 23:49:32 +0900

    Update 'My Profile' text, its styles, and remove border from profile image layout

--- one_day_fe_react/src/Profile.js
+++ one_day_fe_react/src/Profile.js
@@ -149,7 +149,7 @@ const Profile = ({ show, onClose }) => { // Accept show and onClose props
     return (
         <Modal show={show} onClose={onClose}> {/* Wrap content in Modal */}
             <div className="profile-container">
-                <h1>프로필 설정</h1>
+                <h1>My Profile</h1>
                 <div
                     className="profile-picture-container"
                     style={{ backgroundImage: previewImage ? `url(${previewImage})` : 'none' }}
